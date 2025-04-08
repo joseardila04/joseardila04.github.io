c85f961d48e9cacc29e1d8d947aea5c6b45392c9
joseardila04 <joseardila@utexas.edu>
2025-04-08 00:20:55 -0500

    Create App.js

---
+++ assets/js/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import UvMonitor from './components/UvMonitor';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Novara UV Monitor</h1>
+      </header>
+      <UvMonitor />
+    </div>
+  );
+}
+
+export default App;
